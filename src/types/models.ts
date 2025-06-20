051d716cff5e8a43c327c085ada270524ce4cf74
youtaug <youtaig277@gmail.com>
2025-06-20 13:39:44 +0900

    Update models.ts

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,1 +1,20 @@
+export interface Word {
+  id: string;
+  text: string;
+  bbox: [number, number, number, number]; // x,y,w,h @ scale 1
+  stamps: string[];
+}
+export interface PageData {
+  number: number;
+  width: number;
+  height: number;
+  words: Word[];
+}
+export interface AnnotatorFile {
+  app: 'pdf-annotator';
+  version: 1;
+  created: string;
+  pages: PageData[];
+  customStamps: string[];
+}
 
