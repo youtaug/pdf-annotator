2c1a9a22772e304c57a5b5b925a031dea36eb42d
youtaug <youtaig277@gmail.com>
2025-06-20 13:40:55 +0900

    Update usePdfDisplay.ts

--- src/hooks/usePdfDisplay.ts
+++ src/hooks/usePdfDisplay.ts
@@ -1,1 +1,24 @@
+import { getDocument, GlobalWorkerOptions, PDFPageProxy } from 'pdfjs-dist';
+
+GlobalWorkerOptions.workerSrc =
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.2.67/pdf.worker.min.js';
+
+export async function renderPageToCanvas(
+  buf: ArrayBuffer,
+  pageNum: number,
+  canvas: HTMLCanvasElement
+) {
+  const pdf = await getDocument({ data: buf }).promise;
+  const page = await pdf.getPage(pageNum);
+  await internalRender(page, canvas);
+}
+
+async function internalRender(page: PDFPageProxy, canvas: HTMLCanvasElement) {
+  const viewport = page.getViewport({ scale: 1 });
+  const ctx = canvas.getContext('2d')!;
+  canvas.width = viewport.width;
+  canvas.height = viewport.height;
+
+  await page.render({ canvasContext: ctx, viewport }).promise;
+}
 
